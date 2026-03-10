16ada5b75f219e0477f1e72e7945a9709325ca55
Lluc <74605251+LlucMH@users.noreply.github.com>
2026-03-11 00:06:26 +0100

    dates helpers

--- macros/helpers/dates.sql
+++ macros/helpers/dates.sql
@@ -5,11 +5,3 @@
 {% macro days_from_now(n_days) %}
   {{ dbt_checks.dateadd_days(dbt_checks.current_date_sql(), n_days) }}
 {% endmacro %}
-
-{% macro days_ago(n_days) %}
-  {{ dbt_checks.dateadd_days(dbt_checks.current_date_sql(), -1 * n_days) }}
-{% endmacro %}
-
-{% macro days_from_now(n_days) %}
-  {{ dbt_checks.dateadd_days(dbt_checks.current_date_sql(), n_days) }}
-{% endmacro %}
