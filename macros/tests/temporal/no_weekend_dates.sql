fcee924e32c0498d48b0a0d827fc13753b8d4bf5
Lluc <74605251+LlucMH@users.noreply.github.com>
2026-03-25 23:42:52 +0100

    fix no weekend test

--- macros/tests/temporal/no_weekend_dates.sql
+++ macros/tests/temporal/no_weekend_dates.sql
@@ -1,9 +1,9 @@
-{% test recent_date(model, column_name, max_age_days) %}
+{% test no_weekend_dates(model, column_name) %}
 
 select *
 from {{ model }}
 where
     {{ column_name }} is not null
-    and {{ column_name }} < {{ dbt_checks.dateadd_days(dbt_checks.current_date_sql(), -1 * max_age_days) }}
+    and {{ dbt_checks.day_of_week_sun0(column_name) }} in (0, 6)
 
 {% endtest %}
