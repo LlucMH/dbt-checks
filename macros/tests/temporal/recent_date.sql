b061c913eeaf9bc471079c84fab1a0c2426e4c1c
Lluc <74605251+LlucMH@users.noreply.github.com>
2026-03-25 23:38:06 +0100

    fix recent date test

--- macros/tests/temporal/recent_date.sql
+++ macros/tests/temporal/recent_date.sql
@@ -1,9 +1,9 @@
-{% test not_future_date(model, column_name) %}
+{% test recent_date(model, column_name, max_age_days) %}
 
 select *
 from {{ model }}
 where
     {{ column_name }} is not null
-    and {{ column_name }} > {{ dbt_checks.current_date_sql() }}
+    and {{ column_name }} < {{ dbt_checks.dateadd_days(dbt_checks.current_date_sql(), -1 * max_age_days) }}
 
 {% endtest %}
