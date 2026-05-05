810a7c494e020c11d7236d8feabd2219b4eefd0f
Lluc <74605251+LlucMH@users.noreply.github.com>
2026-05-06 00:36:25 +0200

    Fix date_diff helpers

--- macros/tests/temporal/date_diff_less_than.sql
+++ macros/tests/temporal/date_diff_less_than.sql
@@ -2,8 +2,8 @@
 
 with base as (
     select
-        cast({{ start_column }} as date) as start_date,
-        cast({{ end_column }} as date) as end_date
+        cast({{ start_column }} as date) as check_start_date,
+        cast({{ end_column }} as date) as check_end_date
     from {{ model }}
     {% if where %}
     where {{ where }}
@@ -12,17 +12,17 @@ with base as (
 
 validation as (
     select
-        start_date,
-        end_date,
-        {{ dbt_checks.datediff_days('start_date', 'end_date') }} as diff_days
+        check_start_date,
+        check_end_date,
+        {{ dbt_checks.datediff_days('check_start_date', 'check_end_date') }} as diff_days
     from base
 )
 
 select *
 from validation
 where
-    start_date is not null
-    and end_date is not null
+    check_start_date is not null
+    and check_end_date is not null
     and diff_days > {{ max_days }}
 
 {% endtest %}
