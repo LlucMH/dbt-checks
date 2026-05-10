f5ea406980daac504d13b332b853b0876c5511b4
Lluc <74605251+LlucMH@users.noreply.github.com>
2026-05-10 15:48:54 +0200

    Add ratio bounds validation guards to test macros

--- macros/tests/ratio/value_ratio_between.sql
+++ macros/tests/ratio/value_ratio_between.sql
@@ -1,5 +1,7 @@
 {% test value_ratio_between(model, column_name, value, min_ratio, max_ratio, where=None) %}
 
+{{ dbt_checks.validate_ratio_bounds(min_ratio, max_ratio) }}
+
 with
 {{ dbt_checks.calculate_ratio_cte(
     model,
