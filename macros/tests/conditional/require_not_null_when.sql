2c0cec349fdec13f1de81553bd159a4d062da6f2
Lluc <74605251+LlucMH@users.noreply.github.com>
2026-05-16 21:44:06 +0200

    Add conditional validation checks

---
+++ macros/tests/conditional/require_not_null_when.sql
@@ -0,0 +1,21 @@
+{% test require_not_null_when(model, when, column_name, where=None) %}
+
+{{ dbt_checks.validate_required_string(when, 'when') }}
+{{ dbt_checks.validate_required_column(column_name, 'column_name') }}
+
+select
+    *,
+    'require_not_null_when' as failed_check,
+    'Column must not be null when condition is met' as failure_reason,
+    {{ dbt_checks.applied_condition(where) }} as applied_condition
+
+from {{ model }}
+
+where
+    (
+        {{ when }}
+    )
+    and {{ column_name }} is null
+    {{ dbt_checks.apply_and_where(where) }}
+
+{% endtest %}
