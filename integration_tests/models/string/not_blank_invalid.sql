2dc916c30efd8b395df0ff54fa0244316d32da6f
Lluc <74605251+LlucMH@users.noreply.github.com>
2026-07-13 20:30:00 +0200

    Refactor not_blank_invalid model and remove seed data

--- integration_tests/models/string/not_blank_invalid.sql
+++ integration_tests/models/string/not_blank_invalid.sql
@@ -1,1 +1,15 @@
-select * from {{ ref('seed_not_blank_invalid') }}
+select
+    1 as id,
+    cast('hello' as {{ dbt.type_string() }}) as value
+union all
+select
+    2 as id,
+    cast('   ' as {{ dbt.type_string() }}) as value
+union all
+select
+    3 as id,
+    cast('world' as {{ dbt.type_string() }}) as value
+union all
+select
+    4 as id,
+    cast(' ' as {{ dbt.type_string() }}) as value
